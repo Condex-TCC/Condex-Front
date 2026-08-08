16644f12d1794563bdf5df5110472553ca478fbe
Murilo <MuriloT_Costa@hotmail.com>
2026-08-08 14:05:55 -0300

    Fazendo o card para exibir os usuários

--- src/pages/sindico/telaExibeUsuarios.jsx
+++ src/pages/sindico/telaExibeUsuarios.jsx
@@ -1,5 +1,6 @@
 //Página de usuários
 
+import CardUsusario from "../../components/sindico/cardUsuariosSindico"
 import styles from "../../css/telaExibeUsuarios.module.css"
 
 //Função que cria os componentes
@@ -37,27 +38,10 @@ function PaginaExibeUsuarios(){
 
         {/* Elementos do corpo da tabela */}
         <tbody>
-          <tr>
-            <td className={styles.td}>Ana Beatriz Rocha</td>
-            <td className={styles.td}>Morador</td>
-            <td className={styles.td}>48377563898</td>
-            <td className={styles.td}>(23) 637884452</td>
-            <td className={styles.td}>Bloco A - 101</td>
-            <td className={styles.tdCenter}>
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#555" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-              </svg>
-            </td>
-            <td className={styles.tdCenter}>
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#c0392b" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <polyline points="3 6 5 6 21 6"></polyline>
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                <line x1="10" y1="11" x2="10" y2="17"></line>
-                <line x1="14" y1="11" x2="14" y2="17"></line>
-              </svg>
-            </td>
-          </tr>
+          
+            <CardUsusario></CardUsusario>
+
+
         </tbody>
       </table>
     </div>
